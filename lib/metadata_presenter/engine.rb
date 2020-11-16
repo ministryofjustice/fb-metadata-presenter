aadb6a3ecd9fbdacfd2d491624288f915568ea03
Natalie Seeto <njseeto@users.noreply.github.com>
2020-11-16 15:19:09 +0000

    Add formatted Start button and bullet points.
    Use 'govspeak' gem to help with translations.

--- lib/metadata_presenter/engine.rb
+++ lib/metadata_presenter/engine.rb
@@ -1,5 +1,12 @@
 module MetadataPresenter
   class Engine < ::Rails::Engine
     isolate_namespace MetadataPresenter
+
+    initializer 'local_helper.action_controller' do
+      ActiveSupport.on_load :action_controller do
+        helper MetadataPresenter::ApplicationHelper
+      end
+    end
+
   end
 end
--- metadata_presenter.gemspec
+++ metadata_presenter.gemspec
@@ -17,5 +17,6 @@ Gem::Specification.new do |spec|
   spec.files = Dir["{app,config,db,lib}/**/*", "MIT-LICENSE", "Rakefile", "README.md"]
 
   spec.add_dependency "rails", "~> 6.0.3", ">= 6.0.3.4"
+  spec.add_dependency "govspeak"
   spec.add_development_dependency "sqlite3"
 end
