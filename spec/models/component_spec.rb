17f2ac0eb9d3837a292e5592e343f987a3df8d48
Tomas D'Stefano <tomas_stefano@successoft.com>
2020-12-14 16:52:28 -0300

    Add tests for the component class

---
+++ spec/models/component_spec.rb
@@ -0,0 +1,11 @@
+RSpec.describe MetadataPresenter::Component do
+  subject(:component) { described_class.new(attributes) }
+
+  describe '#to_partial_path' do
+    let(:attributes) { { '_type' => 'email' } }
+
+    it 'returns type' do
+      expect(component.to_partial_path).to eq('component/email')
+    end
+  end
+end
