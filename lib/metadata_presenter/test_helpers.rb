382e312ccbbe6e8d80509cfec6484d13b1b473b3
Tomas D'Stefano <tomas_stefano@successoft.com>
2021-01-18 14:28:23 -0300

    Move test helper to be required by other apps
    This is useful to use the metadata as fixture and to be centralised in
    one place.
    
    Co-authored-by: Natalie Seeto <natalie.seeto@digital.justice.gov.uk>
    Co-authored-by: Brendan Butler <brendan.butler@digital.justice.gov.uk>

---
+++ lib/metadata_presenter/test_helpers.rb
@@ -0,0 +1,21 @@
+module MetadataPresenter
+  module TestHelpers
+    def service
+      MetadataPresenter::Service.new(service_metadata)
+    end
+
+    def fixtures_directory
+      @_fixtures_directory ||=
+        Pathname.new(MetadataPresenter::Engine.root.join('fixtures'))
+    end
+
+    def service_metadata
+      metadata_fixture(:version)
+      JSON.parse(File.read(fixtures_directory.join('version.json')))
+    end
+
+    def metadata_fixture(fixture_name)
+      JSON.parse(File.read(fixtures_directory.join("#{fixture_name}.json")))
+    end
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,8 @@ require File.expand_path("dummy/config/environment.rb", __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 Bundler.require(:development, :test)
+require 'metadata_presenter/test_helpers'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -61,5 +63,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  config.include TestHelper
+  config.include MetadataPresenter::TestHelpers
 end
