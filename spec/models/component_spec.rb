53352faf25bae31945f35787ce06404762e3a8df
brenetic <brendan.butler@digital.justice.gov.uk>
2021-02-12 18:07:30 +0000

    Handle radio button components in metadata
    This adds the ability for the presenter to handle the radio buttons
    component as well as validate it as a required field.
    
    We added the humanised_title method in order to be able to correctly
    choose between using a label or legend property for page
    headings/questions, the check your answers page, as well as the key when
    creating the errors hash on the page answers object.

--- spec/models/component_spec.rb
+++ spec/models/component_spec.rb
@@ -10,4 +10,44 @@ RSpec.describe MetadataPresenter::Component do
       )
     end
   end
+
+  describe '#items' do
+    let(:component) do
+      service.find_page_by_url('do-you-like-star-wars').components.first
+    end
+
+    it 'returns an array of openstruct component item objects' do
+      expect(component.items).to be_an(Array)
+    end
+
+    it 'contains objects that respond to the necessary properties' do
+      component.items.each do |item|
+        expect(item).to respond_to(:id)
+        expect(item).to respond_to(:name)
+        expect(item).to respond_to(:description)
+      end
+    end
+  end
+
+  describe '#humanised_title' do
+    context 'when the component has a label' do
+      let(:component) do
+        service.find_page_by_url('name').components.first
+      end
+
+      it 'returns the label value' do
+        expect(component.humanised_title).to eq('Full name')
+      end
+    end
+
+    context 'when the component has a legend' do
+      let(:component) do
+        service.find_page_by_url('do-you-like-star-wars').components.first
+      end
+
+      it 'returns the legend value' do
+        expect(component.humanised_title).to eq('Do you like Star Wars?')
+      end
+    end
+  end
 end
