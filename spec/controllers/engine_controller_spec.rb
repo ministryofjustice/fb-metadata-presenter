08317794116e87d838fe438271fa3a1f0c3e2230
Tomas D'Stefano <tomas_stefano@successoft.com>
2021-02-16 14:06:56 -0300

    Make back link and service name to work in editor preview
    When a user clicks on the preview button on the page flow page or the
    preview link from the forms page a new tab is opened in their browser.
    
    The preview will function as if it is on the runner and therefore the
    back links needed to be adjusted to accommodate this.
    
    Co-authored-by: Natalie Seeto <natalie.seeto@digital.justice.gov.uk>
    Co-authored-by: Brendan Butler <brendan.butler@digital.justice.gov.uk>

--- spec/controllers/engine_controller_spec.rb
+++ spec/controllers/engine_controller_spec.rb
@@ -1,20 +1,54 @@
 RSpec.describe MetadataPresenter::EngineController, type: :controller do
   describe '#back_link' do
-    context 'when there is a page' do
-      let(:page) { MetadataPresenter::Page.new(service.pages.second) }
+    before do
+      allow(controller.request).to receive(:script_name).and_return(script_name)
+    end
+
+    context 'when in preview' do
+      let(:script_name) do
+        '/services/1/preview'
+      end
 
       before do
         controller.instance_variable_set(:@page, page)
       end
 
-      it 'returns the previous page' do
-        expect(controller.back_link).to eq('/')
+      context 'when there is a page' do
+        let(:page) { MetadataPresenter::Page.new(service.pages.second) }
+
+        it 'returns the previous page' do
+          expect(controller.back_link).to eq('/services/1/preview/')
+        end
+      end
+
+      context 'when there is no page' do
+        let(:page) { MetadataPresenter::Page.new(service.pages.first) }
+
+        it 'returns nil' do
+          expect(controller.back_link).to be_nil
+        end
       end
     end
 
-    context 'when there is no page' do
-      it 'returns nil' do
-        expect(controller.back_link).to be_nil
+    context 'when in the runner' do
+      let(:script_name) { '' }
+
+      context 'when there is a page' do
+        let(:page) { MetadataPresenter::Page.new(service.pages.second) }
+
+        before do
+          controller.instance_variable_set(:@page, page)
+        end
+
+        it 'returns the previous page' do
+          expect(controller.back_link).to eq('/')
+        end
+      end
+
+      context 'when there is no page' do
+        it 'returns nil' do
+          expect(controller.back_link).to be_nil
+        end
       end
     end
   end
