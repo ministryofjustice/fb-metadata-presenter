ec9c5bd7b915229c4bd5958581b40cf2f2cf740e
Natalie Seeto <njseeto@users.noreply.github.com>
2021-05-12 15:32:28 +0100

    Add default hint text to upload component and rubocop fixes
    Co-authored-by: Tomas Destefi <tomas.destefi@digital.justice.gov.uk>
    Co-authored-by: Matt Tei <matt.tei@digital.justice.gov.uk>
    Co-authored by: Brendan Butler <brendan.butler@digital.justice.gov.uk>

--- app/validators/metadata_presenter/accept_validator.rb
+++ app/validators/metadata_presenter/accept_validator.rb
@@ -1,6 +1,5 @@
 module MetadataPresenter
   class AcceptValidator < BaseValidator
-    def invalid_answer?
-    end
+    def invalid_answer?; end
   end
 end
--- app/validators/metadata_presenter/max_size_validator.rb
+++ app/validators/metadata_presenter/max_size_validator.rb
@@ -1,6 +1,5 @@
 module MetadataPresenter
   class MaxSizeValidator < BaseValidator
-    def invalid_answer?
-    end
+    def invalid_answer?; end
   end
 end
