7361b7999d8475ae7198482077e27674205259d3
Tomas D'Stefano <tomas_stefano@successoft.com>
2021-06-30 10:33:49 -0300

    Do not return a back link if the user is on the start page

--- app/models/metadata_presenter/previous_page.rb
+++ app/models/metadata_presenter/previous_page.rb
@@ -4,14 +4,12 @@ module MetadataPresenter
     attr_accessor :service, :user_data, :current_page, :referer
 
     def page
-      return if current_page.blank?
+      return if current_page.blank? || current_page == service.start_page
 
       if flow.present?
-        previous_page = TraversedPages.new(service, user_data).last
+        previous_page = TraversedPages.new(service, user_data, current_page).last
 
         return previous_page if previous_page
-
-        service.start_page
       else
         service.previous_page(
           current_page: current_page,
