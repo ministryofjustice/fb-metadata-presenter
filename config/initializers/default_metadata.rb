3311f05c5f97ffc406c880fa43c142c7689169d4
brenetic <brendan.butler@digital.justice.gov.uk>
2021-07-08 13:46:30 +0100

    Fall back to using the '_type' property for default metadata
    Some metadata objects do not have an '_id' property. In those instances
    fallback to using the '_type' property when loading the metadata on
    startup.
    
    Examples of metadata that do not have '_id' properties are the flow
    objects.

--- config/initializers/default_metadata.rb
+++ config/initializers/default_metadata.rb
@@ -6,8 +6,11 @@ Rails.logger.info('Loading default metadata')
 default_metadata = Dir.glob("#{Rails.application.config.default_metadata_directory}/*/**")
 default_metadata.each do |metadata_file|
   metadata = JSON.parse(File.read(metadata_file))
-  Rails.logger.info(metadata['_id'])
-  Rails.application.config.default_metadata[metadata['_id']] = metadata
+
+  key = metadata['_id'] || metadata['_type']
+  Rails.logger.info(key)
+
+  Rails.application.config.default_metadata[key] = metadata
 end
 
 Rails.logger.info(
