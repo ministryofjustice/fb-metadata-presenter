d8a4e27f1eb0d26a97cee000d5a003b77e4c94ed
Natalie Seeto <njseeto@users.noreply.github.com>
2022-02-08 17:20:51 +0000

    CYA and Confirmation page column placement
    During grid placement, check whether a check answers page exists.
    If it exists, ensure the confirmation page is in the next column.
    
    Co-authored-by: Brendan Butler <brendan.butler@digital.justice.gov.uk>

--- app/models/metadata_presenter/column_number.rb
+++ app/models/metadata_presenter/column_number.rb
@@ -25,18 +25,35 @@ module MetadataPresenter
 
     def column_number
       @column_number ||= begin
-        return latest_column if cya_or_confirmation_page?
+        return last_column if confirmation_page?
+        return latest_column if checkanswers_page?
 
         existing_column || new_column
       end
+    end
+
+    def confirmation_page?
+      service.confirmation_page&.uuid == uuid
+    end
+
+    def checkanswers_page?
+      service.checkanswers_page&.uuid == uuid
     end
 
     def latest_column
       [existing_column, new_column].compact.max
     end
 
-    def cya_or_confirmation_page?
-      [service.checkanswers_page&.uuid, service.confirmation_page&.uuid].include?(uuid)
+    def last_column
+      checkanswers_column.present? ? checkanswers_column + 1 : latest_column
+    end
+
+    def checkanswers_column
+      @checkanswers_column ||= begin
+        return if service.checkanswers_page.blank?
+
+        coordinates.uuid_column(service.checkanswers_page&.uuid)
+      end
     end
 
     def existing_column
