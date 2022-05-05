d383a0231ad62b2d16f2588c0a714738fcc9737c
brenetic <brendan.butler@digital.justice.gov.uk>
2022-05-05 12:11:17 +0100

    Add sub definitions to schema initialiser
    If any schema has definitions internally then iterate over those and
    also add them as inidividually retrievable schemas when doing
    validation.

--- config/initializers/schemas.rb
+++ config/initializers/schemas.rb
@@ -8,6 +8,15 @@ schemas.each do |schema_file|
   Rails.logger.info(schema['_name'])
   jschema = JSON::Schema.new(schema, Addressable::URI.parse(schema['_name']))
   JSON::Validator.add_schema(jschema)
+
+  if schema.key?('definitions')
+    schema['definitions'].each do |key, definition|
+      definition_key = "#{schema['_name']}.#{key}"
+      Rails.logger.info(definition_key)
+      jschema = JSON::Schema.new(definition, Addressable::URI.parse(definition_key))
+      JSON::Validator.add_schema(jschema)
+    end
+  end
 end
 
 Rails.logger.info("Total loaded schemas => #{JSON::Validator.schemas.count}")
