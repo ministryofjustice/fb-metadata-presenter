3293fe9ad3b8ab4118e26c3a5c5f29c5d78179dc
Chris Pymm <chris@chrispymm.co.uk>
2022-12-01 14:24:23 +0000

    Appease rubocop in Circle

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,9 +34,8 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  config.fixture_path = ::Rails.root.join('spec', 'fixtures')
+  # If you're not using ActiveRecord, or youud prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
