d1698c46294cee584f14f86b249b3dbd6b125885
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-03-24 10:44:31 +0000

    Add super basic spec around model to create foundation, fix cancel button

--- app/models/metadata_presenter/saved_form.rb
+++ app/models/metadata_presenter/saved_form.rb
@@ -2,8 +2,11 @@ module MetadataPresenter
   class SavedForm
     include ActiveModel::Model
 
+    attr_accessor :email,
+                  :secret_question,
+                  :secret_answer
+
     def initialize
-      
     end
 
     def email
---
+++ spec/models/saved_form_spec.rb
@@ -0,0 +1,7 @@
+RSpec.describe MetadataPresenter::SavedForm do
+  describe 'initialize' do
+    it 'should create successfully' do
+      expect(subject.email).to eq(nil)
+    end
+  end
+end
