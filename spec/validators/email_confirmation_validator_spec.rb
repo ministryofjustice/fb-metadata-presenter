daa23980656356280feba23695f60cf183ca2072
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-04-03 16:31:47 +0100

    Add validator specs

---
+++ spec/validators/email_confirmation_validator_spec.rb
@@ -0,0 +1,15 @@
+RSpec.describe SavedProgressValidator do
+  describe '#valid?' do
+    let (:record) { MetadataPresenter::EmailConfirmation.new }
+
+    it 'is valid when both attributes match' do
+      record.assign_attributes('123', '123')
+      expect(record.valid?).to be(true)
+    end
+    
+    it 'is not valid if the attributes do not match' do
+      record.assign_attributes('123', '456')
+      expect(record.valid?).to be(false)
+    end
+  end
+end
