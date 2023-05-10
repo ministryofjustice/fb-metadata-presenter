3bdac8d3c1a0e394932963f8635e646f844c7164
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-05-10 14:45:15 +0100

    Appease the cop

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -3,7 +3,7 @@ RSpec.describe 'Answers Controller Requests', type: :request do
     it 'saves and redirects if the param is set' do
       expect_any_instance_of(MetadataPresenter::AnswersController).to receive(:save_user_data)
       post '/', params: { save_for_later: true }
-      
+
       expect(response).to redirect_to('/save')
     end
   end
@@ -12,7 +12,7 @@ RSpec.describe 'Answers Controller Requests', type: :request do
     it 'does not create a submission from check your answers' do
       expect_any_instance_of(MetadataPresenter::SubmissionsController).to_not receive(:create_submission)
       post '/reserved/submissions', params: { save_for_later_check_answers: true }
-      
+
       expect(response).to redirect_to('/save')
     end
   end
