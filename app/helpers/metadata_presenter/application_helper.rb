5f28ec9d330f54d78a0fed9c4d1e1a6acf4d9768
Chris Pymm <chris@chrispymm.co.uk>
2023-05-18 15:38:15 +0100

    Changes to add editable-content web component

--- app/helpers/metadata_presenter/application_helper.rb
+++ app/helpers/metadata_presenter/application_helper.rb
@@ -12,7 +12,8 @@ module MetadataPresenter
     #   <%=to_html '# Some markdown' %>
     #
     def to_html(text)
-      Kramdown::Document.new(text).to_html.html_safe
+      # Kramdown::Document.new(text).to_html.html_safe
+      Govspeak::Document.new(text).to_html.html_safe
     end
 
     def default_text(property)
