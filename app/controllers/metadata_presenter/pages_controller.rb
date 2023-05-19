0067b1141e8486aa013887ef2afcad245f67d1d2
Chris Pymm <chris@chrispymm.co.uk>
2023-05-19 12:50:19 +0100

    set cache-controlheader

--- app/controllers/metadata_presenter/pages_controller.rb
+++ app/controllers/metadata_presenter/pages_controller.rb
@@ -1,5 +1,8 @@
 module MetadataPresenter
+
   class PagesController < EngineController
+    before_action :set_caching_header
+
     def show
       @user_data = load_user_data # method signature
       @page ||= service.find_page_by_url(request.env['PATH_INFO'])
@@ -8,10 +11,6 @@ module MetadataPresenter
         load_autocomplete_items
 
         @page_answers = PageAnswers.new(@page, @user_data)
-
-        if(@page.template == 'metadata_presenter/page/checkanswers')
-          response.headers["Cache-Control"] = "private, no-store"
-        end
 
         render template: @page.template
       else
@@ -30,6 +29,12 @@ module MetadataPresenter
 
     def answered_pages
       TraversedPages.new(service, load_user_data, @page).all
+    end
+
+    private
+
+    def set_caching_header
+      response.headers["Cache-Control"] = "no-store"
     end
   end
 end
