76f4c264909dd382f087de5e83f37e5357998ff0
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-05-23 09:56:56 +0100

    Rails 7 upgrade

--- spec/dummy/config/initializers/filter_parameter_logging.rb
+++ spec/dummy/config/initializers/filter_parameter_logging.rb
@@ -1,4 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
-# Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+# Configure parameters to be filtered from the log file. Use this to limit dissemination of
+# sensitive information. See the ActiveSupport::ParameterFilter documentation for supported
+# notations and behaviors.
+# rubocop:disable Style/SymbolArray
+Rails.application.config.filter_parameters += [
+  :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :password
+]
+# rubocop:enable Style/SymbolArray
