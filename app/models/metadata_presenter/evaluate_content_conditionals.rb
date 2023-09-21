4e25364e2efc7b20df023bfa7d21dcdd70d900ce
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-09-21 14:21:19 +0100

    Removing previous automated tests from previous implementation

--- app/models/metadata_presenter/evaluate_content_conditionals.rb
+++ app/models/metadata_presenter/evaluate_content_conditionals.rb
@@ -3,10 +3,6 @@ module MetadataPresenter
     include ActiveModel::Model
     attr_accessor :service, :candidate_component, :user_data
 
-    def show_component
-      component_uuid
-    end
-
     def uuids_to_include
       included_content = []
       conditionals.map do |conditional|
@@ -37,28 +33,6 @@ module MetadataPresenter
         end
       end
       included_content
-    end
-
-    def component_uuid
-      @results ||=
-        conditionals.map do |conditional|
-          evaluated_expressions = conditional.expressions.map do |expression|
-            expression.service = service
-            Operator.new(
-              expression.operator
-            ).evaluate(
-              expression.field_label,
-              user_data[expression.expression_component.id]
-            )
-          end
-
-          if conditional.type == 'or' && evaluated_expressions.any?
-            candidate_component.uuid
-          elsif evaluated_expressions.all?
-            candidate_component.uuid
-          end
-        end
-      @results.flatten.compact.first
     end
 
     delegate :conditionals, to: :candidate_component
