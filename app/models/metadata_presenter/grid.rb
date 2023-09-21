081cd0470793909039d220c8e46dd1450d8b25c5
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-09-21 13:42:22 +0100

    Wehat about 1000

--- app/models/metadata_presenter/grid.rb
+++ app/models/metadata_presenter/grid.rb
@@ -137,7 +137,7 @@ module MetadataPresenter
       Rails.logger.info("Total potential routes: #{total_potential_routes}")
 
       until routes_to_traverse.empty?
-        if index > total_potential_routes || index = 200
+        if index > total_potential_routes || index = 1000
           ActiveSupport::Notifications.instrument(
             'exceeded_total_potential_routes',
             message: 'Exceeded total number of potential routes'
