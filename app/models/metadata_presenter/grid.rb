828e699d4e53320bcfa973b57dc82d0532bf59a6
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2023-09-21 15:20:16 +0100

    ?

--- app/models/metadata_presenter/grid.rb
+++ app/models/metadata_presenter/grid.rb
@@ -137,7 +137,7 @@ module MetadataPresenter
       Rails.logger.info("Total potential routes: #{total_potential_routes}")
 
       until routes_to_traverse.empty?
-        if index > total_potential_routes || index = 1000
+        if index > total_potential_routes || index = total_potential_routes/2
           ActiveSupport::Notifications.instrument(
             'exceeded_total_potential_routes',
             message: 'Exceeded total number of potential routes'
