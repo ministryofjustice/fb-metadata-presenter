aa3c37a3b867b619aa0c48b949a87ee3624aea41
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2023-09-26 10:55:28 +0100

    Adding handling of AND rule for conditional content

--- app/models/metadata_presenter/evaluate_content_conditionals.rb
+++ app/models/metadata_presenter/evaluate_content_conditionals.rb
@@ -2,33 +2,33 @@ module MetadataPresenter
   class EvaluateContentConditionals
     include ActiveModel::Model
     attr_accessor :service, :candidate_component, :user_data
-
+    
     def uuids_to_include
-      included_content = []
       conditionals.map do |conditional|
+        evaluation_expressions = []
         conditional.expressions.map do |expression|
           expression.service = service
           case expression.operator
           when 'is'
             if expression.field_label == user_data[expression.expression_component.id]
-              included_content << candidate_component.uuid
+              evaluation_expressions << true
             end
           when 'is_not'
             if expression.field_label != user_data[expression.expression_component.id]
-              included_content << candidate_component.uuid
+              evaluation_expressions << true
             end
           when 'is_answered'
             if user_data[expression.expression_component.id].present?
-              included_content << candidate_component.uuid
+              evaluation_expressions << true
             end
           when 'is_not_answered'
             if user_data[expression.expression_component.id].blank?
-              included_content << candidate_component.uuid
+              evaluation_expressions << true
             end
           end
         end
+        return candidate_component.uuid if evaluation_expressions.all?
       end
-      included_content
     end
 
     delegate :conditionals, to: :candidate_component
