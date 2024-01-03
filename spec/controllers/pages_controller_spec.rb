4a46ebd01ea87210bd71f9310f92600182be514e
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2024-01-03 13:06:40 +0000

    Adding test cases when page exists in page controller

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -10,4 +10,37 @@ RSpec.describe MetadataPresenter::PagesController do
       controller.pages_presenters
     end
   end
+
+  describe '#show' do
+    context 'when page exists' do
+      let(:page) { double(MetadataPresenter::Page) }
+      let(:template_page) { 'metadata_presenter/page/start' }
+      let(:a_response) { {status: 'OK'} }
+
+      before do
+        allow(page).to receive(:template).and_return(template_page)
+        allow(page).to receive(:autocomplete_component_present?).and_return(false)
+        allow(page).to receive(:load_all_content)
+        allow(page).to receive(:load_conditional_content)
+        controller.instance_variable_set(:@page, page)
+        allow_any_instance_of(ActionController::Instrumentation).to receive(:render).and_return(a_response)
+      end
+
+      it 'renders a template with conditional content' do
+        expect(controller).to receive(:render)
+        controller.show
+      end
+    end
+
+    context 'when page does not exist' do
+      before do
+        allow_any_instance_of(MetadataPresenter::Service).to receive(:find_page_by_url).and_return(nil)
+      end
+
+      it 'renders the not found template' do
+        expect(controller).to receive(:not_found)
+        controller.show
+      end
+    end
+  end
 end
