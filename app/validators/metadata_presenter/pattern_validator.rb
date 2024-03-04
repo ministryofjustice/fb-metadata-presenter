59508c229056e8daf602b61911e401b9c4170f9e
H3ll3m4 <hellema.ibrahim@digital.justice.gov.uk>
2024-03-04 09:21:36 +0000

    Fixing validator

--- app/validators/metadata_presenter/pattern_validator.rb
+++ app/validators/metadata_presenter/pattern_validator.rb
@@ -1,7 +1,7 @@
 module MetadataPresenter
   class PatternValidator < BaseValidator
     def invalid_answer?
-      regex = component.validation[schema_key].to_i
+      regex = component.validation[schema_key]
       !user_answer.to_s.match(regex)
     end
   end
