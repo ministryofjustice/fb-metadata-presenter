3744dde28cd5369eb3f3765661a882f87027a14f
Steven Leighton <steven.leighton@digital.justice.gov.uk>
2024-06-26 12:00:43 +0100

    WIPgp

--- app/controllers/metadata_presenter/pages_controller.rb
+++ app/controllers/metadata_presenter/pages_controller.rb
@@ -48,17 +48,17 @@ module MetadataPresenter
 
     def form_page_title
       if @page
-        if @page.heading.present?
-          if @page['_type'] == 'page.standalone' && @page['_id'] == 'page.cookies'
-            "#{service.service_name} - #{@page.heading}"
-          else
-            "#{service.service_name} - #{@page.heading}"
-          end
-        elsif @page.components.present?
+        if @page.components.present?
           if @page.components.first['label'].present?
             "#{service.service_name} - #{@page.components.first['label']}"
           elsif @page.components.first['legend'].present?
             "#{service.service_name} - #{@page.components.first['legend']}"
+          end
+        elsif @page.heading.present?
+          if @page['_type'] == 'page.standalone' && @page['_id'] == 'page.cookies'
+            "#{service.service_name} - #{@page.heading}"
+          else
+            "#{service.service_name} - #{@page.heading}"
           end
         else
           service.service_name || 'MoJ Forms'
