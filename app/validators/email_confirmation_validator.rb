5fffc343096b5dc33c28f514b0c22027f5844fab
matt.tei <matt.tei@digital.justice.gov.uk>
2026-01-08 10:52:38 +0000

    Changed the email validation to use a custom REGEX
    The ruby URI::MailTo::EMAIL_REGEXP allowed name@5 as a valid email address.
    
    The old REGEX is from /.rbenv/versions/3.2.0/lib/ruby/3.2.0/uri/mailto.rb
    
    With the new REGEX being custom.
    
    Co-authored-by: Ripan Kumar  <ripan.kumar@justice.gov.uk>

--- app/validators/email_confirmation_validator.rb
+++ app/validators/email_confirmation_validator.rb
@@ -1,6 +1,6 @@
 class EmailConfirmationValidator < ActiveModel::Validator
   def validate(record)
-    regex = URI::MailTo::EMAIL_REGEXP
+    regex = EMAIL_REGEXP
     if record.email_confirmation.match(regex).nil?
       record.errors.add(:email_confirmation, I18n.t('presenter.save_and_return.validation.email'))
     end
