ba79a7fc4c2e7b7a6f4f027b65ff1187b5f59967
Ripan Kumar <er.ripankumar@gmail.com>
2026-04-16 18:23:24 +0100

    Fix rubocop suggestion

--- app/models/metadata_presenter/page_answers.rb
+++ app/models/metadata_presenter/page_answers.rb
@@ -32,7 +32,7 @@ module MetadataPresenter
         upload_answer(component.id, count)
       elsif component && component.type == 'multiupload'
         answer_object = multiupload_answer(component.id, count)
-        answer_object.to_h if answer_object.present?
+        answer_object.presence&.to_h
       elsif component && component.type == 'checkboxes'
         answers[method_name.to_s].to_a
       elsif component && component.type == 'address'
