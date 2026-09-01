332fd23bf045d33548f63fd0f7391f46961b68cc
matt tei <matthew.tei@justice.gov.uk>
2026-09-01 14:19:55 +0100

    Changed controller to remove duplicates.

--- app/controllers/metadata_presenter/pages_controller.rb
+++ app/controllers/metadata_presenter/pages_controller.rb
@@ -68,10 +68,6 @@ module MetadataPresenter
     private
 
     def set_caching_header
-      response.headers['Cache-Control'] = 'no-store'
-    end
-
-    def set_caching_header
       response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate'
       response.headers['Pragma'] = 'no-cache'
       response.headers['Expires'] = '0'
